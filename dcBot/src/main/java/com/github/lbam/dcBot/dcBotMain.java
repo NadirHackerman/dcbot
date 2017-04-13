e0c30d3a0ef8a2c37867da7c001292bb66229f61
lucasbarross <lbam@cin.ufpe.br>
2017-04-13 12:24:57 -0300

    Começo

---
+++ dcBot/src/main/java/com/github/lbam/dcBot/dcBotMain.java
@@ -0,0 +1,13 @@
+package com.github.lbam.dcBot;
+
+/**
+ * Hello world!
+ *
+ */
+public class dcBotMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
