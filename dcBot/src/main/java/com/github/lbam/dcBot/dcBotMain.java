c092a60383fa4e8df08c11534c11ce0f1f3cf710
lucasbarross <lbam@cin.ufpe.br>
2017-04-13 16:13:47 -0300

    Main class

--- dcBot/src/main/java/com/github/lbam/dcBot/dcBotMain.java
+++ dcBot/src/main/java/com/github/lbam/dcBot/dcBotMain.java
@@ -1,13 +1,21 @@
 package com.github.lbam.dcBot;
 
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.util.DiscordException;
+
 /**
- * Hello world!
+ * Classe de inicialização
  *
  */
-public class dcBotMain
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class dcBotMain{
+	public static IDiscordClient Bot;
+    
+	public static void main(String[] args){
+    	Bot = getClient(Token);
+    }
+    
+    public static IDiscordClient getClient(String Token) throws DiscordException{
+		return new ClientBuilder().withToken(Token).login();
     }
 }
